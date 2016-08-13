5d2114981f44669728059671c74efd37e08ff6ed
MaxMEllon <maxmellon1994@gmail.com>
2016-08-13 15:41:37 +0900

    Feature: mouse move can out of DOM

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, PropType } from 'react';
+
+const isFunc = obj => typeof obj === 'function';
+
+const calcWidth = (client, cursor, max) => {
+  const width = parseInt(client - cursor);
+  if (width >= max) return max;
+  if (width <= 0) return 0;
+  return width;
+};
 
 export default class Slider extends Component {
+  static get PropTypes() {
+    return {
+      max: PropType.number.isRequired,
+      width: PropType.number.isRequired,
+      height: PropType.number.isRequired,
+      backStyle: PropType.object,
+      frontStyle: PropType.object,
+      onChange:  PropType.func,
+    };
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -12,22 +32,29 @@ export default class Slider extends Component {
     this.onMouseUp = this.onMouseUp.bind(this);
   }
 
+  componentDidMount() {
+    window.addEventListener('mousemove', this.onMouseMove);
+    window.addEventListener('mouseup', this.onMouseUp);
+  }
+
+  componentDidUpdate() {
+    const value = parseInt(this.state.width / this.props.width * this.props.max);
+    if (isFunc(this.props.onChange)) this.props.onChange(event, value);
+  }
+
   onMouseDown(event) {
-    const targetLeft = event.target.offsetLeft;
-    this.setState({
-      width: `${event.clientX - targetLeft}px`,
-      drag: true,
-    });
+    const width = calcWidth(event.clientX, event.target.offsetLeft, this.props.width);
+    this.setState({ width, drag: true, });
   }
 
   onMouseMove(event) {
     if (!this.state.drag) return;
-    const targetLeft = event.target.offsetLeft;
-    this.setState({ width: `${event.clientX - targetLeft}px` });
+    const width = calcWidth(event.clientX, event.target.offsetLeft, this.props.width);
+    this.setState({ width });
   }
 
   onMouseUp() {
-    this.setState({ drag: false });
+    if (this.state.drag) this.setState({ drag: false });
   }
 
   render() {
@@ -36,13 +63,10 @@ export default class Slider extends Component {
         <div
           className="react-simple-slider"
           onMouseDown={this.onMouseDown}
-          onMouseMove={this.onMouseMove}
           onMouseUp={this.onMouseUp}
           style={{
-            marginLeft: '20px',
-            marginTop: '90px',
-            width: '100px',
-            height: '10px',
+            width: this.props.width,
+            height: this.props.height,
             backgroundColor: 'gray',
           }}>
           <div
--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Slider from './components/Slider';
 
+const onChange = (event, value) => {
+  console.log(value);
+};
+
 window.onload = function() {
-  ReactDOM.render(<Slider />, document.getElementById('main'));
+  ReactDOM.render(
+    <div>
+      <Slider
+        height={20}
+        width={300}
+        max={100}
+        onChange={onChange}
+      />
+
+      <Slider
+        height={10}
+        width={400}
+        max={100}
+        onChange={onChange}
+      />
+    </div>
+    ,
+    document.getElementById('main')
+  );
   if (module.hot) {
     module.hot.accept(function(err) {
       if (err) console.error(err);
     });
   }
-}
+};
 
 // vim: ft=javascript.jsx
 
